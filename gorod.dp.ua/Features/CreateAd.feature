c63a94a3669b95dd053aac0f8020f58d0ab6b651
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-10-21 11:08:36 +0300

    Add project files.

---
+++ gorod.dp.ua/Features/CreateAd.feature
@@ -0,0 +1,21 @@
+﻿Feature: Advertisement
+	In order to gain money from sells
+	As a user of gorod.dp.ua website
+	I want to manage my advertisements on the gorod.dp.ua
+
+	Background: 
+	Given I on the ad page
+	When I want to login to my account with my mcsymiv 123456789
+
+@createAd
+Scenario: Successful create car sell ad
+	Given I want to add my ad
+	When I fill in all necessary fields
+	When I click on add ad
+	Then I see thank you message on the page
+
+@error_on_submit
+Scenario: Error on skipping necessary fields
+	Given Ad form is open
+	When User leaves necessary field empty
+	Then User see red error message
---
+++ gorod.dp.ua/Steps/AdvertisementSteps.cs
@@ -0,0 +1,82 @@
+﻿using gorod.dp.ua.POMs;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using TechTalk.SpecFlow;
+
+namespace gorod.dp.ua.Steps
+{
+    [Binding]
+    public class AdvertisementSteps
+    {
+        IWebDriver chrome;
+        AdvertisementPageObject ad_page;
+        AdFormPage form_page;
+        [Given(@"I on the ad page")]
+        public void GivenIOnTheAdPage()
+        {
+            chrome = new ChromeDriver(@"C:\Users\mcsymiv\Desktop\git\chromedriver_win32");
+            chrome.Navigate().GoToUrl("https://gorod.dp.ua/gazeta/");
+            ad_page = new AdvertisementPageObject(chrome);
+            form_page = new AdFormPage(chrome);
+            chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [When(@"I want to login to my account with my (.*) (.*)")]
+        public void WhenIWantToLoginToMyAccountWithMy(string login, string password)
+        {
+            ad_page.UserAuthorize(login, password);
+        }
+
+        [Given(@"I want to add my ad")]
+        public void GivenIWantToAddMyAd()
+        {
+            ad_page.OpenCarAdForm();
+        }
+
+        [When(@"I fill in all necessary fields")]
+        public void WhenIFillInAllNecessaryFields()
+        {
+            form_page.FillNecessaryFieldsAd("продать", "2000", "30000", "2000", "Отличное состояние");
+        }
+
+        [When(@"I click on add ad")]
+        public void WhenIClickOnAddAd()
+        {
+            form_page.SubmitAdForm();
+        }
+
+        [Then(@"I see thank you message on the page")]
+        public void ThenISeeThankYouMessageOnThePage()
+        {
+            string msg = ad_page.ThankYouMessage();
+            Assert.IsTrue(msg.Contains("добавлено и открыто после проверки администратором"));
+        }
+
+        [Given(@"Ad form is open")]
+        public void GivenAdFormIsOpen()
+        {
+            ad_page.OpenCarAdForm();
+        }
+
+        [When(@"User leaves necessary field empty")]
+        public void WhenUserLeavesNecessaryFieldEmpty()
+        {
+            form_page.SubmitAdForm();
+        }
+
+        [Then(@"User see red error message")]
+        public void ThenUserSeeRedErrorMessage()
+        {
+            string errorMsg = ad_page.ErrorMessage();
+            Assert.IsTrue(errorMsg.Contains("Ошибка"));
+        }
+        [AfterScenario]
+        public void ClosePage()
+        {
+            chrome.Quit();
+        }
+    }
+}
